ebf8175e92cee9d2bf422b7adf68f6ebf5be3f90
CrazyUOuOb <96434813+CrazyUOuOb@users.noreply.github.com>
2025-04-06 11:42:58 +0800

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,28 @@
+<?php
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+
+try {
+    $conn = mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
+} catch (Exception $e) {
+    echo '<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Online blogging platform</title>
+        <link rel="stylesheet" type="text/css" href="indexstyle.css">
+    </head>
+    <body>
+        <div class="container" style="text-align: center; padding: 50px;">
+            <h1>Database is Waking Up</h1>
+            <p>Please wait a moment and refresh the page.</p>
+            <p><small>The database might need a few seconds to become available. This is normal for hosted services.</small></p>
+        </div>
+    </body>
+    </html>';
+    exit();
+}
+
+?>
