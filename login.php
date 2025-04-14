6489243c5c7822240903c47d88950578077b46c1
CrazyUOuOb <96434813+CrazyUOuOb@users.noreply.github.com>
2025-04-14 20:16:44 +0800

    Update login.php

--- login.php
+++ login.php
@@ -26,6 +26,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
+        <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECF51EJ15B"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-ECF51EJ15B');
+    </script>
+    <title>Online blogging platform</title>
+    <link rel="stylesheet" type="text/css" href="indexstyle.css">
+    
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
