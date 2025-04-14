cc440c06017eea70b6374d9dfa09d3d41137f634
CrazyUOuOb <96434813+CrazyUOuOb@users.noreply.github.com>
2025-04-14 23:06:21 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -11,6 +11,15 @@ try {
     echo '<!DOCTYPE html>
     <html>
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECF51EJ15B"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ECF51EJ15B');
+        </script>
+    
         <title>Online blogging platform</title>
         <link rel="stylesheet" type="text/css" href="indexstyle.css">
     </head>
