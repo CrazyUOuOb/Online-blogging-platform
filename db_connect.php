34e228f98010c532c49dfcad6fb537edbaf542ad
CrazyUOuOb <96434813+CrazyUOuOb@users.noreply.github.com>
2025-04-16 16:27:49 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -13,12 +13,13 @@ try {
     <head>
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECF51EJ15B"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-ECF51EJ15B');
-        </script>
+
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-ECF51EJ15B');
+    </script>
     
         <title>Online blogging platform</title>
         <link rel="stylesheet" type="text/css" href="indexstyle.css">
