2462757f88269c08c155b0bf5ba39a25b2c6571e
CrazyUOuOb <96434813+CrazyUOuOb@users.noreply.github.com>
2025-04-20 08:57:42 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
-setcookie("user_id", "", time() - 3600, "/"); 
+# Delete the cookie by setting its expiration time to the past
+setcookie("user_id", "", time() - 3600, "/");  
 
-header("Location: index.php"); 
+header("Location: index.php"); # Redirect to the home page after logout
 exit();
 ?>
